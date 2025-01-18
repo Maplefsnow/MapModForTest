457f926ef089dbd94cc1be5a2718a639cd5bfcef
Maplef <maplefsnow@gmail.com>
2025-01-18 16:36:56 +0000

    able to use, version 0.1

--- src/main/java/me/maplef/mapmodfortest/PlayerListener.java
+++ src/main/java/me/maplef/mapmodfortest/PlayerListener.java
@@ -1,27 +1,15 @@
 package me.maplef.mapmodfortest;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.ServerChatEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class PlayerListener {
     @SubscribeEvent
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getSide().isServer()) return;
-        LogUtils.getLogger().info("Player interact!");
-        Player player = event.getEntity();
-        player.sendSystemMessage(Component.literal("weeeeeeeeeeeeeeeeeeeee"));
-    }
-
-    @SubscribeEvent
     public void onPlayerChat(ServerChatEvent event) {
         long chat_id = -1002439035442L;
         try {
-            TGBotManager.getInstance().sendMessage(chat_id, event.getUsername() + ": " + event.getMessage());
+            TGBotManager.getInstance().sendMessage(chat_id, event.getUsername() + ": " + event.getMessage().getString());
         } catch (TelegramApiException ex) {
             ex.printStackTrace();
         }
--- src/main/java/me/maplef/mapmodfortest/TGListener.java
+++ src/main/java/me/maplef/mapmodfortest/TGListener.java
@@ -1,11 +1,15 @@
 package me.maplef.mapmodfortest;
 
 import com.mojang.logging.LogUtils;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 public class TGListener implements LongPollingSingleThreadUpdateConsumer {
@@ -23,7 +27,12 @@ public class TGListener implements LongPollingSingleThreadUpdateConsumer {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
 
-            LogUtils.getLogger().info(String.format("Received from chat_%ld: %s", chat_id, message_text));
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                player.sendSystemMessage(Component.literal("[TG] " + update.getMessage().getFrom().getUserName() + ": " + message_text));
+            }
+
+            LogUtils.getLogger().info(String.format("Received from chat_%s: %s", String.valueOf(chat_id), message_text));
         }
     }
 }
